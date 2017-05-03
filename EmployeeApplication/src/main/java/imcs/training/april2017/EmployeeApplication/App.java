508ab794ce488a4f9e9d24a54ca9952d230a2a7b
Manoj <manojreddy.pothuganti@gmail.com>
2017-05-03 03:13:03 -0500

    This is main application to run the employee application operations

---
+++ EmployeeApplication/src/main/java/imcs/training/april2017/EmployeeApplication/App.java
@@ -0,0 +1,13 @@
+package imcs.training.april2017.EmployeeApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
